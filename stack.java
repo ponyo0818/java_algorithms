87447a1f0e36952e486e2bacaf56e6a9ab3fcb25
Yuan Fang <43736431+ponyo0818@users.noreply.github.com>
2019-03-25 10:01:36 +0800

    Add files via upload

---
+++ stack.java
@@ -0,0 +1,14 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+public class stack{
+	public static void main(String[] args)
+	{
+		StackOfStrings stack = new StackOfStrings();
+		while (!StdIn.isEmpty())
+		{
+			String s = StdIn.readString();
+			if (s.equals("-")) StdOut.print(stack.pop());
+			else stack.push(s);
+		}
+	}
+}
